1b3293074d26cb7716bd159dc9521234be51ab19
Md Aasif <mdaasifaslam786@gmail.com>
2021-11-05 18:42:44 +0530

    Add:Clock.

---
+++ makingClock.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+class clock{
+private:
+    int h,m,s,err,a;
+public:
+    clock(void){
+        h=m=s=err=a=0;
+    }
+    void setDisplayTime(void){
+        while(err==0){
+            cout<<"Enter hour:";
+            cin>>h;
+            cout<<"Enter minute:";
+            cin>>m;
+            cout<<"Enter second:";
+            cin>>s;
+            if(h<=24&&m<60&&s<60){
+                err++;
+            }
+            else{
+                cout<<"you have put wrong time.\n";
+                system("cls");
+            }
+        }
+        while(a==0){
+            system("cls");
+            cout<<h<<":"<<m<<":"<<s<<"\n";
+            Sleep(1000);
+            s++;
+            if(s>59){
+                m++;
+                s=0;
+            }
+            if(m>59){
+                h++;
+                m=0;
+            }
+            if(h>24){
+                h=1;
+            }
+        }
+    }
+};
+int main(void){
+clock cl;
+    cl.setDisplayTime();
+return 0;
+}
